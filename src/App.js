60bded73ea5fee16da264a1189bd37213fd958de
Chichi Zhang <chichizhang20000510@gmail.com>
2025-05-15 18:13:15 -0700

    change chichi branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn react
+          I was changed into chichi branch
         </a>
       </header>
     </div>
